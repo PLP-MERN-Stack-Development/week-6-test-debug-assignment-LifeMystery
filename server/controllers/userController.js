0203320273c17c80f2cb348739b14d99b488ef3d
Lebo <162670180+LifeMystery@users.noreply.github.com>
2025-07-24 21:20:40 +0200

    Create userController.js

---
+++ server/controllers/userController.js
@@ -0,0 +1,8 @@
+const User = require('../models/userModel');
+
+exports.registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
+  const user = new User({ username, email, password });
+  await user.save();
+  res.status(201).json({ message: 'User registered successfully' });
+};
